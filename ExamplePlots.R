ec8a0e403d2b63ae28e7073d4d125553a16af76f
Beasley015 <beasley.em@gmail.com>
2019-12-23 09:37:19 -0500

    finished biased estimate example plot

---
+++ ExamplePlots.R
@@ -0,0 +1,25 @@
+#######################################################
+# Plot of hypothetical scenarios                      #
+# Fig. 1 of proposal                                  #
+# Spring 2020                                         #
+#######################################################
+
+# Load package
+library(ggplot2)
+
+# Write function for logistic curve
+curve <- function(x){
+  y <- 10*(x /(15 + abs(x)))
+  return(y)
+}
+
+# Make the plot
+bias.plot <- ggplot()+
+  geom_abline(slope = 0.5, intercept = 0, size = 1)+
+  geom_abline(slope = 0.48, intercept = -0.5, linetype = 2, size = 1)+
+  stat_function(aes(-10:10), fun = curve, n = 100, linetype = 3, size = 1)+
+  labs(x = "Observed", y = "Estimated")+
+  theme_classic(base_size = 18)+
+  theme(axis.text = element_blank())
+
+ggsave(bias.plot, file = "bias.jpeg", dpi = 650)
